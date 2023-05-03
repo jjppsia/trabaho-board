dfc7616a4735a0a8f23f398d109c060c5b322461
Jp Sia <johnpaulsia4@gmail.com>
2023-05-03 20:37:24 +0800

    feat: data persists in local storage

--- src/state/app-state-context.tsx
+++ src/state/app-state-context.tsx
@@ -1,33 +1,12 @@
 import { Dispatch, createContext, useContext } from 'react'
-import { useImmerReducer } from 'use-immer'
 
-import { DragItem } from '@/types'
+import { useLocalStorageWithReducer } from '@/hooks/use-local-storage-with-reducer'
 import { Action } from '@/state/actions'
-import {
-  AppState,
-  List,
-  Task,
-  appStateReducer,
-} from '@/state/app-state-reducer'
+import { AppState, List, Task } from '@/state/app-state-reducer'
+import { DragItem } from '@/types'
 
-const appData: AppState = {
-  lists: [
-    {
-      id: '1',
-      text: 'To Do',
-      tasks: [{ id: 'c0', text: 'Implement oauth authentication' }],
-    },
-    {
-      id: '2',
-      text: 'In Progress',
-      tasks: [{ id: 'c1', text: 'Move columns and rows back and forth' }],
-    },
-    {
-      id: '3',
-      text: 'Done',
-      tasks: [{ id: 'c2', text: 'Refactor imports. It must use aliases' }],
-    },
-  ],
+export const initialAppState: AppState = {
+  lists: [],
   draggedItem: null,
 }
 
@@ -43,8 +22,8 @@ const AppStateContext = createContext<AppStateContextProps>(
 )
 
 function AppStateProvider({ children }: { children: React.ReactNode }) {
-  const [state, dispatch] = useImmerReducer(appStateReducer, appData)
-  const { lists, draggedItem } = state
+  const [appState, dispatch] = useLocalStorageWithReducer(initialAppState)
+  const { lists, draggedItem } = appState
 
   const getTasksByListId = (id: string) => {
     return lists.find((list) => list.id === id)?.tasks || []
